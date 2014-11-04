8e1e0359fa858d4d5798070c201f5f35644bc32a
ironhank <rhmushinski@gmail.com>
2014-11-04 12:46:23 -0500

    first commit

---
+++ setUp_MySQLdb.sh
@@ -0,0 +1,11 @@
+echo "Setting Up MYSQL For Python"
+
+pip install MySQL-python
+
+echo "MySQL for Python installed"
+
+echo "Setting Libraries"
+
+ln -s /usr/local/mysql/lib/libmysqlclient.18.dylib /usr/lib/libmysqlclient.18.dylib
+
+echo "Libraries set"
